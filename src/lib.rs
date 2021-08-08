89deb401a950076a82c6bed16cfcfefdc9beadf4
Trevor Wilson <kmdreko@gmail.com>
2021-08-08 00:04:15 -0600

    get new function-based approach working

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,17 @@ use std::collections::HashMap;
 use std::hash::Hash;
 use std::ops::{Deref, DerefMut};
 
+use serde::de::Error;
 use serde::ser::SerializeStruct;
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
-use crate::private::KeyValueVisitor;
+use crate::private::{KeyValueVisitor, KeyValueVisitor2};
 
 mod private;
 
-pub type DeserializationFn<T> =
-    Box<dyn Fn(&mut dyn erased_serde::Deserializer) -> Result<T, erased_serde::Error>>;
+pub type DeserializationFn<T> = Box<
+    dyn Fn(&mut dyn erased_serde::Deserializer) -> Result<T, erased_serde::Error> + Send + Sync,
+>;
 
 pub struct DeserializationMap<K, V> {
     type_name: &'static str,
@@ -53,14 +55,17 @@ where
 
     pub fn insert_fn<F>(&mut self, key: K, f: F)
     where
-        F: Fn(&mut dyn erased_serde::Deserializer) -> Result<V, erased_serde::Error> + 'static,
+        F: Fn(&mut dyn erased_serde::Deserializer) -> Result<V, erased_serde::Error>
+            + 'static
+            + Send
+            + Sync,
     {
         self.insert(key, Box::new(f));
     }
 
     pub fn insert_unit_fn<F>(&mut self, key: K, f: F)
     where
-        F: Fn() -> Result<V, erased_serde::Error> + 'static,
+        F: Fn() -> Result<V, erased_serde::Error> + 'static + Send + Sync,
     {
         self.insert(
             key,
@@ -80,6 +85,70 @@ macro_rules! insert_fn_boxed {
         $crate::DeserializationMap::insert_fn($map, $key, |deserializer| {
             $f(deserializer).map(|v| Box::new(v) as _)
         })
+    };
+}
+
+#[macro_export]
+macro_rules! deserialize_into_boxed_trait {
+    ($ty:ty) => {
+        Box::new(|deserializer| <$ty>::deserialize(deserializer).map(|v| Box::new(v) as Box<_>))
+    };
+}
+
+#[macro_export]
+macro_rules! impl_serialize {
+    ($key_name:ident : $key_type:ty, $data_name:ident : $data_type:ty, $f:expr) => {
+        pub fn serialize<S>(data: &$data_type, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: ::serde::Serializer,
+        {
+            use ::serde::ser::SerializeStruct;
+
+            struct ErasedSerdeWrapper<'a, V: ?Sized>(&'a V);
+            impl<'a, V: ?Sized> ::serde::Serialize for ErasedSerdeWrapper<'a, V>
+            where
+                V: ::erased_serde::Serialize,
+            {
+                fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+                where
+                    S: ::serde::Serializer,
+                {
+                    erased_serde::serialize(self.0, serializer)
+                }
+            }
+
+            fn f<F: Fn(&$data_type) -> $key_type>(f: F) -> F {
+                f
+            }
+
+            let f = f($f);
+
+            let mut state = serializer.serialize_struct(std::stringify!($data_type), 2)?;
+            state.serialize_field(std::stringify!($key_name), &f(data))?;
+            state.serialize_field(std::stringify!($data_name), &ErasedSerdeWrapper(&**data))?;
+            state.end()
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! impl_deserialize {
+    ($key_name:ident : $key_type:ty, $data_name:ident : $data_type:ty, $f:expr) => {
+        pub fn deserialize<'de, D>(deserializer: D) -> Result<$data_type, D::Error>
+        where
+            D: ::serde::Deserializer<'de>,
+        {
+            deserializer.deserialize_struct(
+                std::stringify!($data_type),
+                &[std::stringify!($key_name), std::stringify!($data_name)],
+                crate::private::KeyValueVisitor2::<_, $key_type, $data_type> {
+                    deserialization_fn: $f,
+                    key_name: std::stringify!($key_name),
+                    value_name: std::stringify!($data_name),
+                    _dummy: std::marker::PhantomData,
+                },
+            )
+        }
     };
 }
 
@@ -148,6 +217,64 @@ where
         state.serialize_field(self.field_names[1], &ValueWrapper(v))?;
         state.end()
     }
+}
+
+pub fn serialize_with_key<S, K, V>(
+    type_name: &'static str,
+    field_names: &'static [&'static str; 2],
+    key: &K,
+    value: &V,
+    serializer: S,
+) -> Result<S::Ok, S::Error>
+where
+    K: ?Sized + ::serde::Serialize,
+    V: ?Sized + ::erased_serde::Serialize,
+    S: Serializer,
+{
+    struct ErasedSerdeSerializeWrapper<'a, V: ?Sized>(&'a V);
+    impl<'a, V: ?Sized> Serialize for ErasedSerdeSerializeWrapper<'a, V>
+    where
+        V: erased_serde::Serialize,
+    {
+        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: Serializer,
+        {
+            erased_serde::serialize(self.0, serializer)
+        }
+    }
+
+    let mut state = serializer.serialize_struct(type_name, 2)?;
+    state.serialize_field(field_names[0], key)?;
+    state.serialize_field(field_names[1], &ErasedSerdeSerializeWrapper(value))?;
+    state.end()
+}
+
+pub fn deserialize_by_key<'de, D, K, V, F>(
+    type_name: &'static str,
+    field_names: &'static [&'static str; 2],
+    f: F,
+    deserializer: D,
+) -> Result<V, D::Error>
+where
+    D: Deserializer<'de>,
+    K: Deserialize<'de>,
+    F: Fn(K, &mut dyn ::erased_serde::Deserializer) -> Result<V, ::erased_serde::Error>,
+{
+    deserializer.deserialize_struct(
+        type_name,
+        field_names,
+        KeyValueVisitor2 {
+            deserialization_fn: f,
+            key_name: field_names[0],
+            value_name: field_names[1],
+            _dummy: std::marker::PhantomData,
+        },
+    )
+}
+
+pub fn unknown_key() -> erased_serde::Error {
+    erased_serde::Error::custom("unknown deserialization key")
 }
 
 #[cfg(test)]
@@ -315,56 +442,73 @@ mod tests {
 
     #[test]
     fn ergonomics() {
-        fn deserialize_box_dyn_testtrait<'de, D>(
-            deserializer: D,
-        ) -> Result<Box<dyn TestTrait>, D::Error>
-        where
-            D: Deserializer<'de>,
-        {
-            static TestDeserializationMap: DeserializationMap<String, Box<dyn TestTrait>> = {
-                let mut map = DeserializationMap::new("Box<dyn TestTrait>", &["id", "data"]);
+        use serde::{Deserialize, Serialize};
 
-                insert_fn_boxed!(&mut map, "A".to_string(), TestStructA::deserialize);
-                insert_fn_boxed!(&mut map, "B".to_string(), TestEnumB::deserialize);
-                insert_fn_boxed!(&mut map, "C".to_string(), TestUnitC::deserialize);
+        mod test_trait {
+            use crate as desfn;
 
-                map
-            };
+            use std::collections::HashMap;
+            use std::ops::Deref;
+            use std::sync::RwLock;
 
-            TestDeserializationMap.deserialize_by_key(deserializer)
-        }
+            use desfn::{deserialize_into_boxed_trait, DeserializationFn};
+            use once_cell::sync::Lazy;
+            use serde::{Deserialize, Deserializer, Serializer};
 
-        fn serialize_box_dyn_testtrait<S>(
-            v: Box<dyn TestTrait>,
-            serializer: S,
-        ) -> Result<S::Ok, S::Error>
-        where
-            S: Serializer,
-        {
-            struct ValueWrapper<'a, V: ?Sized>(&'a V);
-            impl<'a, V: ?Sized> Serialize for ValueWrapper<'a, V>
+            use super::tests::{TestEnumB, TestStructA, TestTrait, TestUnitC};
+
+            static MAP: Lazy<RwLock<HashMap<String, DeserializationFn<Box<dyn TestTrait>>>>> =
+                Lazy::new(|| {
+                    let mut map = HashMap::<String, DeserializationFn<Box<dyn TestTrait>>>::new();
+
+                    map.insert("A".to_string(), deserialize_into_boxed_trait!(TestStructA));
+                    map.insert("B".to_string(), deserialize_into_boxed_trait!(TestEnumB));
+                    map.insert("C".to_string(), deserialize_into_boxed_trait!(TestUnitC));
+
+                    RwLock::new(map)
+                });
+
+            pub(super) fn serialize<S>(
+                value: &Box<dyn TestTrait>,
+                serializer: S,
+            ) -> Result<S::Ok, S::Error>
             where
-                V: erased_serde::Serialize,
+                S: Serializer,
             {
-                fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-                where
-                    S: Serializer,
-                {
-                    erased_serde::serialize(self.0, serializer)
-                }
+                desfn::serialize_with_key(
+                    "Box<dyn TestTrait>",
+                    &["id", "data"],
+                    value.key(),
+                    value.deref(),
+                    serializer,
+                )
             }
 
-            let v = v.as_ref();
-            let mut state = serializer.serialize_struct("", 2)?;
-            state.serialize_field(self.field_names[0], &(self.key_fn)(v))?;
-            state.serialize_field(self.field_names[1], &ValueWrapper(v))?;
-            state.end()
+            pub(super) fn deserialize<'de, D>(
+                deserializer: D,
+            ) -> Result<Box<dyn TestTrait>, D::Error>
+            where
+                D: Deserializer<'de>,
+            {
+                desfn::deserialize_by_key(
+                    "Box<dyn TestTrait>",
+                    &["id", "data"],
+                    |key: String, deserializer| {
+                        MAP.read()
+                            .unwrap()
+                            .get(&key)
+                            .ok_or_else(desfn::unknown_key)
+                            .and_then(|f| f(deserializer))
+                    },
+                    deserializer,
+                )
+            }
         }
 
         #[derive(Serialize, Deserialize)]
         struct Wrapper {
-            #[serde(deserialize_with = "deserialize_box_dyn_testtrait")]
-            value: Box<dyn TestTrait>,
+            #[serde(with = "test_trait")]
+            test: Box<dyn TestTrait>,
         }
     }
 }
--- src/private.rs
+++ src/private.rs
@@ -302,3 +302,253 @@ where
         }
     }
 }
+
+struct DataDeserializeSeed2<'a, F, K, T>
+where
+    F: Fn(K, &mut dyn erased_serde::Deserializer) -> Result<T, erased_serde::Error>,
+{
+    field: K,
+    deserialization_fn: &'a F,
+    _dummy: PhantomData<fn(K) -> T>,
+}
+
+impl<'de, 'a, F, K, T> DeserializeSeed<'de> for DataDeserializeSeed2<'a, F, K, T>
+where
+    F: Fn(K, &mut dyn erased_serde::Deserializer) -> Result<T, erased_serde::Error>,
+{
+    type Value = T;
+    fn deserialize<D>(self, deserializer: D) -> Result<Self::Value, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        (self.deserialization_fn)(
+            self.field,
+            &mut <dyn erased_serde::Deserializer>::erase(deserializer),
+        )
+        .map_err(D::Error::custom)
+    }
+}
+
+pub struct KeyValueVisitor2<F, K, T>
+where
+    F: Fn(K, &mut dyn erased_serde::Deserializer) -> Result<T, erased_serde::Error>,
+{
+    pub deserialization_fn: F,
+    pub key_name: &'static str,
+    pub value_name: &'static str,
+    pub _dummy: PhantomData<fn(K) -> T>,
+}
+
+impl<'de, F, K, T> Visitor<'de> for KeyValueVisitor2<F, K, T>
+where
+    F: Fn(K, &mut dyn erased_serde::Deserializer) -> Result<T, erased_serde::Error>,
+    K: Deserialize<'de>,
+{
+    type Value = T;
+    fn expecting(&self, __formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        std::fmt::Formatter::write_str(__formatter, "adjacently tagged enum")
+    }
+    fn visit_map<A>(self, mut map: A) -> Result<Self::Value, A::Error>
+    where
+        A: MapAccess<'de>,
+    {
+        match {
+            let mut __rk: Option<TagOrContentField> = None;
+            while let Some(__k) = map.next_key_seed(TagContentOtherFieldVisitor {
+                tag: self.key_name,
+                content: self.value_name,
+            })? {
+                match __k {
+                    TagContentOtherField::Other => {
+                        map.next_value::<IgnoredAny>()?;
+                        continue;
+                    }
+                    TagContentOtherField::Tag => {
+                        __rk = Some(TagOrContentField::Tag);
+                        break;
+                    }
+                    TagContentOtherField::Content => {
+                        __rk = Some(TagOrContentField::Content);
+                        break;
+                    }
+                }
+            }
+            __rk
+        } {
+            Some(TagOrContentField::Tag) => {
+                let __field = map.next_value()?;
+                match {
+                    let mut __rk: Option<TagOrContentField> = None;
+                    while let Some(__k) = map.next_key_seed(TagContentOtherFieldVisitor {
+                        tag: self.key_name,
+                        content: self.value_name,
+                    })? {
+                        match __k {
+                            TagContentOtherField::Other => {
+                                map.next_value::<IgnoredAny>()?;
+                                continue;
+                            }
+                            TagContentOtherField::Tag => {
+                                __rk = Some(TagOrContentField::Tag);
+                                break;
+                            }
+                            TagContentOtherField::Content => {
+                                __rk = Some(TagOrContentField::Content);
+                                break;
+                            }
+                        }
+                    }
+                    __rk
+                } {
+                    Some(TagOrContentField::Tag) => Err(
+                        <A::Error as serde::de::Error>::duplicate_field(self.key_name),
+                    ),
+                    Some(TagOrContentField::Content) => {
+                        let __ret = map.next_value_seed(DataDeserializeSeed2 {
+                            field: __field,
+                            deserialization_fn: &self.deserialization_fn,
+                            _dummy: self._dummy,
+                        })?;
+                        match {
+                            let mut __rk: Option<TagOrContentField> = None;
+                            while let Some(__k) =
+                                map.next_key_seed(TagContentOtherFieldVisitor {
+                                    tag: self.key_name,
+                                    content: self.value_name,
+                                })?
+                            {
+                                match __k {
+                                    TagContentOtherField::Other => {
+                                        map.next_value::<IgnoredAny>()?;
+                                        continue;
+                                    }
+                                    TagContentOtherField::Tag => {
+                                        __rk = Some(TagOrContentField::Tag);
+                                        break;
+                                    }
+                                    TagContentOtherField::Content => {
+                                        __rk = Some(TagOrContentField::Content);
+                                        break;
+                                    }
+                                }
+                            }
+                            __rk
+                        } {
+                            Some(TagOrContentField::Tag) => Err(
+                                <A::Error as serde::de::Error>::duplicate_field(self.key_name),
+                            ),
+                            Some(TagOrContentField::Content) => Err(
+                                <A::Error as serde::de::Error>::duplicate_field(self.value_name),
+                            ),
+                            None => Ok(__ret),
+                        }
+                    }
+                    None => {
+                        let __deserializer =
+                            MissingFieldDeserializer::<A::Error>(self.value_name, PhantomData);
+
+                        (self.deserialization_fn)(
+                            __field,
+                            &mut <dyn erased_serde::Deserializer>::erase(__deserializer),
+                        )
+                        .map_err(A::Error::custom)
+                    }
+                }
+            }
+            Some(TagOrContentField::Content) => {
+                let __content = map.next_value::<Content>()?;
+                match {
+                    let mut __rk: Option<TagOrContentField> = None;
+                    while let Some(__k) = map.next_key_seed(TagContentOtherFieldVisitor {
+                        tag: self.key_name,
+                        content: self.value_name,
+                    })? {
+                        match __k {
+                            TagContentOtherField::Other => {
+                                map.next_value::<IgnoredAny>()?;
+                                continue;
+                            }
+                            TagContentOtherField::Tag => {
+                                __rk = Some(TagOrContentField::Tag);
+                                break;
+                            }
+                            TagContentOtherField::Content => {
+                                __rk = Some(TagOrContentField::Content);
+                                break;
+                            }
+                        }
+                    }
+                    __rk
+                } {
+                    Some(TagOrContentField::Tag) => {
+                        let __deserializer = ContentDeserializer::<A::Error>::new(__content);
+                        let __val = map.next_value()?;
+
+                        let __ret = (self.deserialization_fn)(
+                            __val,
+                            &mut <dyn erased_serde::Deserializer>::erase(__deserializer),
+                        )
+                        .map_err(A::Error::custom)?;
+
+                        match {
+                            let mut __rk: Option<TagOrContentField> = None;
+                            while let Some(__k) =
+                                map.next_key_seed(TagContentOtherFieldVisitor {
+                                    tag: self.key_name,
+                                    content: self.value_name,
+                                })?
+                            {
+                                match __k {
+                                    TagContentOtherField::Other => {
+                                        map.next_value::<IgnoredAny>()?;
+                                        continue;
+                                    }
+                                    TagContentOtherField::Tag => {
+                                        __rk = Some(TagOrContentField::Tag);
+                                        break;
+                                    }
+                                    TagContentOtherField::Content => {
+                                        __rk = Some(TagOrContentField::Content);
+                                        break;
+                                    }
+                                }
+                            }
+                            __rk
+                        } {
+                            Some(TagOrContentField::Tag) => Err(
+                                <A::Error as serde::de::Error>::duplicate_field(self.key_name),
+                            ),
+                            Some(TagOrContentField::Content) => Err(
+                                <A::Error as serde::de::Error>::duplicate_field(self.value_name),
+                            ),
+                            None => Ok(__ret),
+                        }
+                    }
+                    Some(TagOrContentField::Content) => Err(
+                        <A::Error as serde::de::Error>::duplicate_field(self.value_name),
+                    ),
+                    None => Err(<A::Error as serde::de::Error>::missing_field(self.key_name)),
+                }
+            }
+            None => Err(<A::Error as serde::de::Error>::missing_field(self.key_name)),
+        }
+    }
+    fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+    where
+        A: SeqAccess<'de>,
+    {
+        match seq.next_element()? {
+            Some(__field) => {
+                match seq.next_element_seed(DataDeserializeSeed2 {
+                    field: __field,
+                    deserialization_fn: &self.deserialization_fn,
+                    _dummy: self._dummy,
+                })? {
+                    Some(__ret) => Ok(__ret),
+                    None => Err(serde::de::Error::invalid_length(1, &self)),
+                }
+            }
+            None => Err(serde::de::Error::invalid_length(0, &self)),
+        }
+    }
+}
